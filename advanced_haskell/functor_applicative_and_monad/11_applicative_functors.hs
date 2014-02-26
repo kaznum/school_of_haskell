2a4c321a0f5c508c24d04dbca9d057d05a00c973
Kazuya NUMATA <numata@gmail.com>
2014-02-26 11:36:30 +0900

    Implement 'Applicative Functors' examples

---
+++ advanced_haskell/functor_applicative_and_monad/11_applicative_functors.hs
@@ -0,0 +1,38 @@
+import Safe (readMay)
+import Control.Applicative((<*>))
+displayAge maybeAge =
+  case maybeAge of
+    Nothing -> putStrLn "You provided invalid input"
+    Just age -> putStrLn $ "In that year, you will be: " ++ show age
+
+yearDiff futureYear birthYear = futureYear - birthYear
+
+main = do
+  putStrLn "Please enter your birth year"
+  birthYearString <- getLine
+  putStrLn "Please enter some year in the future"
+  futureYearString <- getLine
+  let maybeAge = do
+        fmap yearDiff (readMay futureYearString)
+        <*> readMay birthYearString
+  displayAge maybeAge
+
+
+-- *Main> main
+-- Please enter your birth year
+-- 1976
+-- Please enter some year in the future
+-- 2014
+-- In that year, you will be: 38
+-- *Main> main
+-- Please enter your birth year
+-- hello
+-- Please enter some year in the future
+-- 2014
+-- You provided invalid input
+-- *Main> main
+-- Please enter your birth year
+-- 1976
+-- Please enter some year in the future
+-- hello
+-- You provided invalid input
