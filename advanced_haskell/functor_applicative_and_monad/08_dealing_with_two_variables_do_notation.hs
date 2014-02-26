13ae22f4e39c182d187700e0cdb848d2900e440b
Kazuya NUMATA <numata@gmail.com>
2014-02-26 11:11:19 +0900

    Implement 'Dealing with two variables' example with do-notation

---
+++ advanced_haskell/functor_applicative_and_monad/08_dealing_with_two_variables_do_notation.hs
@@ -0,0 +1,38 @@
+import Safe (readMay)
+
+displayAge maybeAge =
+  case maybeAge of
+    Nothing -> putStrLn "You provided invalid input"
+    Just age -> putStrLn $ "In that year, you will be: " ++ show age
+
+yearDiff futureYear birthYear = futureYear - birthYear
+
+main = do
+  putStrLn "Please enter your birth year"
+  yearString <- getLine
+  putStrLn "Please enter some year in the future"
+  futureYearString <- getLine
+  let maybeAge = do
+        birthYear <- readMay yearString
+        futureYear <- readMay futureYearString
+        return $ yearDiff futureYear birthYear
+  displayAge maybeAge
+
+-- *Main> main
+-- Please enter your birth year
+-- 1976
+-- Please enter some year in the future
+-- 2014
+-- In that year, you will be: 38
+-- *Main> main
+-- Please enter your birth year
+-- hello
+-- Please enter some year in the future
+-- 2014
+-- You provided invalid input
+-- *Main> main
+-- Please enter your birth year
+-- 1976
+-- Please enter some year in the future
+-- hello
+-- You provided invalid input
