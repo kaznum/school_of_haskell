2e5446e8b4709fb3ce9f46bb725ee634b7ee46b1
Kazuya NUMATA <numata@gmail.com>
2014-02-26 10:55:26 +0900

    Implement 'Decoupling code' exsample

---
+++ advanced_haskell/functor_applicative_and_monad/04_decoupling_code.hs
@@ -0,0 +1,26 @@
+import Safe (readMay)
+
+displayAge maybeAge =
+  case maybeAge of
+    Nothing -> putStrLn "You provided an invalid year"
+    Just age -> putStrLn $ "In 2020, you will be: " ++ show age
+
+yearToAge year = 2020 - year
+
+main = do
+  putStrLn "Please enter your birth year"
+  yearString <- getLine
+  let maybeAge =
+        case readMay yearString of
+          Nothing -> Nothing
+          Just year -> Just (yearToAge year)
+  displayAge maybeAge
+
+-- *Main> main
+-- Please enter your birth year
+-- 1976
+-- In 2020, you will be: 44
+-- *Main> main
+-- Please enter your birth year
+-- hello
+-- You provided an invalid year
