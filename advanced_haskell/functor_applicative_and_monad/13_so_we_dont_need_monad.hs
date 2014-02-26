5f66f47c08ac1aa27fb99852bf9155c3f8e66301
Kazuya NUMATA <numata@gmail.com>
2014-02-26 11:41:36 +0900

    Implement 'So we don't need monads?' example

---
+++ advanced_haskell/functor_applicative_and_monad/13_so_we_dont_need_monad.hs
@@ -0,0 +1,50 @@
+import Safe (readMay)
+import Control.Applicative((<*>))
+import Control.Applicative((<$>))
+
+displayAge maybeAge =
+  case maybeAge of
+    Nothing -> putStrLn "You provided invalid input"
+    Just age -> putStrLn $ "In that year, you will be: " ++ show age
+
+yearDiff futureYear birthYear = futureYear - birthYear
+
+main = do
+  putStrLn "Please enter your birth year"
+  birthYearString <- getLine
+  putStrLn "Please enter some year in the future"
+  futureYearString <- getLine
+  let maybeAge = do
+        futureYear <- readMay futureYearString
+        birthYear <- readMay birthYearString
+        return $
+          if futureYear < birthYear
+          then yearDiff birthYear futureYear
+          else yearDiff futureYear birthYear
+  displayAge maybeAge
+
+
+-- *Main> main
+-- Please enter your birth year
+-- 1976
+-- Please enter some year in the future
+-- 2014
+-- In that year, you will be: 38
+-- *Main> main
+-- Please enter your birth year
+-- 2014
+-- Please enter some year in the future
+-- 1976
+-- In that year, you will be: 38
+-- *Main> main
+-- Please enter your birth year
+-- hello
+-- Please enter some year in the future
+-- 2014
+-- You provided invalid input
+-- *Main> main
+-- Please enter your birth year
+-- 1976
+-- Please enter some year in the future
+-- hello
+-- You provided invalid input
