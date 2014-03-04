149fa8d7f6ed20b8784bfe385788b9278c7ad23a
Kazuya NUMATA <numata@gmail.com>
2014-03-04 17:50:05 +0900

    Implement 'Separate Markup from Source Code' in Part2

--- advanced_haskell/building_a_file_hosting_service_in_yesod/Main.hs
+++ advanced_haskell/building_a_file_hosting_service_in_yesod/Main.hs
@@ -3,43 +3,34 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
+import Data.Default
 import Yesod
-import Data.String
+import Yesod.Default.Util
 
 data App = App
 
 instance Yesod App
 
-mkYesod "App" [parseRoutes|
-               / HomeR GET
-|]
+-- mkYesod "App" [parseRoutes|
+--               / HomeR GET
+-- |]
+
+mkYesod "App" $(parseRoutesFile "config/routes")
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
---  let filename1 = "readme.txt" :: String
---      filename2 = "report.pdf" :: String
---      filename3 = "music.wav" :: String
---  let filenames = ["readme.txt", "report.pdf", "music.wav"] :: [String]
-  let filenames = [] :: [String]
+  let filenames = ["readme.txt", "report.pdf", "music.wav"] :: [String]
   setTitle "File Processor"
-  toWidget [whamlet|
-<h2>Previously submitted files
-$#<ul>
-$#  <li>readme.txt
-$#  <li>report.pdf
-$#  <li>music.wav
-$#  <li>#{filename1}
-$#  <li>#{filename2}
-$#  <li>#{filename3}
-$#  $forall filename <- filenames
-$#    <li>#{filename}
-$if null filenames
-  <p> No files have been uploaded yet.
-$else
-  <ul>
-    $forall filename <- filenames
-      <li>#{filename}
-|]
+--   toWidget [whamlet|
+-- <h2>Previously submitted files
+-- $if null filenames
+--   <p> No files have been uploaded yet.
+-- $else
+--   <ul>
+--     $forall filename <- filenames
+--       <li>#{filename}
+-- |]
+  $(widgetFileNoReload def "home")
 
 
 main :: IO ()
