7b1fdd21248379130fa8c248dca297fea58a69c8
Kazuya NUMATA <numata@gmail.com>
2014-03-04 16:16:13 +0900

    Implement Pulling Data from Surroundings in Part1

--- advanced_haskell/building_a_file_hosting_service_in_yesod/Main.hs
+++ advanced_haskell/building_a_file_hosting_service_in_yesod/Main.hs
@@ -16,13 +16,19 @@ mkYesod "App" [parseRoutes|
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
+  let filename1 = "readme.txt" :: String
+      filename2 = "report.pdf" :: String
+      filename3 = "music.wav" :: String
   setTitle "File Processor"
   toWidget [whamlet|
 <h2>Previously submitted files
 <ul>
-  <li>readme.txt
-  <li>report.pdf
-  <li>music.wav
+$#  <li>readme.txt
+$#  <li>report.pdf
+$#  <li>music.wav
+  <li>#{filename1}
+  <li>#{filename2}
+  <li>#{filename3}
 |]
 
 
