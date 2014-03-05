7b1cab115ab9ec36dbdeb30baac55fe5ebaa03df
Kazuya NUMATA <numata@gmail.com>
2014-03-05 16:17:48 +0900

    Implement "Global Style Sheets" in Part3

--- advanced_haskell/building_a_file_hosting_service_in_yesod/Foundation.hs
+++ advanced_haskell/building_a_file_hosting_service_in_yesod/Foundation.hs
@@ -7,14 +7,18 @@ module Foundation where
 
 import Control.Concurrent.STM
 import Data.ByteString.Lazy (ByteString)
+import Data.Default
 import Data.Text (Text)
 import qualified Data.Text as Text
-
-
+import Text.Hamlet
 import Yesod
+import Yesod.Default.Util
 
 data App = App (TVar [(Text, ByteString)])
-instance Yesod App
+instance Yesod App where
+  defaultLayout widget = do
+    pc <- widgetToPageContent $ $(widgetFileNoReload def "default-layout")
+    giveUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
 instance RenderMessage App FormMessage where
   renderMessage _ _ = defaultFormMessage
